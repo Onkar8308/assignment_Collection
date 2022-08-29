3a4f378ce167208c53c16116ea605a7d68490adc
Onkar8308 <101888473+Onkar8308@users.noreply.github.com>
2022-08-29 16:39:19 +0530

    Delete demo directory

--- demo/src/demo/Main.java
+++
@@ -1,7 +0,0 @@
-package demo;
-
-public class Main {
-	public static void main(String[] args) {
-		System.out.println("this is my 1st meaven project");
-	}
-}
--- demo/src/main/java/a1.java
+++
@@ -1,8 +0,0 @@
-package main.java;
-
-public class a1 {
-	public static void main(String[] args) {
-		System.out.println("this is my 1st meaven program");
-	}
-
-}
